f2c29d764e0467833d474f536af39fbb60b5d909
Pedro Beirao <pedrocbeirao@gmail.com>
2026-09-19 19:34:04 +0100

    Almost working correctly

---
+++ index.js
@@ -0,0 +1,130 @@
+const choose_wad = document.getElementById("choose_wad");
+
+choose_wad.addEventListener("change", () => {
+  const file = choose_wad.files?.[0];
+
+  if (file)
+    load_wad(file);
+})
+
+function load_wad(file) {
+  const reader = new FileReader();
+
+  reader.onerror = () => {
+    console.log("Error loading WAD");
+  }
+
+  reader.onload = () => {
+    const bytes = new Uint8Array(reader.result);
+    const endoom_offset = find_endoom(bytes);
+    display_endoom(bytes, endoom_offset);
+  }
+
+  reader.readAsArrayBuffer(file);
+}
+
+function read4bytes(bytes, offset) {
+  return (
+    bytes[offset] |
+    (bytes[offset + 1] << 8) |
+    (bytes[offset + 2] << 16) |
+    (bytes[offset + 3] << 24)
+  ) >>> 0;
+}
+
+function decode_string(bytes, offset, size) {
+  var res = "";
+  for (var i = 0; i < size; i++) {
+    res += String.fromCharCode(bytes[offset + i])
+  }
+  return res.replace(/\0/g, "").trim();
+}
+
+function find_endoom(bytes) {
+  const numlumps = read4bytes(bytes, 4);
+  const infotableofs = read4bytes(bytes, 8);
+  var offset = infotableofs;
+
+  for (var i = 0; i < numlumps; i++) {
+    const name = decode_string(bytes, offset+8, 8);
+    if (name == "ENDOOM" || name == "ENDBOOM" || name == "ENDTEXT" || name == "ENDSTRF")
+      return read4bytes(bytes, offset);
+
+    offset += 16;
+  }
+}
+
+const colors = [
+  "#000000", // 0 black
+  "#0000AA", // 1 blue
+  "#00AA00", // 2 green
+  "#00AAAA", // 3 cyan
+  "#AA0000", // 4 red
+  "#AA00AA", // 5 magenta
+  "#AAAA00", // 6 yellow/brown
+  "#AAAAAA", // 7 white
+  "#555555", // 8 dark gray
+  "#5555FF", // 9 bright blue
+  "#55FF55", // A bright green
+  "#55FFFF", // B bright cyan
+  "#FF5555", // C bright red
+  "#FF55FF", // D bright magenta
+  "#FFFF55", // E bright yellow
+  "#FFFFFF"  // F bright white
+];
+const cp437_to_utf8 = [
+  "\u0000","\u263A","\u263B","\u2665","\u2666","\u2663","\u2660","\u2022",
+  "\u25D8","\u25CB","\u25D9","\u2642","\u2640","\u266A","\u266B","\u263C",
+  "\u25BA","\u25C4","\u2195","\u203C","\u00B6","\u00A7","\u25AC","\u21A8",
+  "\u2191","\u2193","\u2192","\u2190","\u221F","\u2194","\u25B2","\u25BC",
+  " ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",
+  "0","1","2","3","4","5","6","7","8","9",":",";","<","=" ,">","?",
+  "@","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
+  "P","Q","R","S","T","U","V","W","X","Y","Z","[","\\","]","^","_",
+  "`","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+  "p","q","r","s","t","u","v","w","x","y","z","{","|","}","~","\u2302",
+  "\u00C7","\u00FC","\u00E9","\u00E2","\u00E4","\u00E0","\u00E5","\u00E7",
+  "\u00EA","\u00EB","\u00E8","\u00EF","\u00EE","\u00EC","\u00C4","\u00C5",
+  "\u00C9","\u00E6","\u00C6","\u00F4","\u00F6","\u00F2","\u00FB","\u00F9",
+  "\u00FF","\u00D6","\u00DC","\u00A2","\u00A3","\u00A5","\u20A7","\u0192",
+  "\u00E1","\u00ED","\u00F3","\u00FA","\u00F1","\u00D1","\u00AA","\u00BA",
+  "\u00BF","\u2310","\u00AC","\u00BD","\u00BC","\u00A1","\u00AB","\u00BB",
+  "\u2591","\u2592","\u2593","\u2502","\u2524","\u2561","\u2562","\u2556",
+  "\u2555","\u2563","\u2551","\u2557","\u255D","\u255C","\u255B","\u2510",
+  "\u2514","\u2534","\u252C","\u251C","\u2500","\u253C","\u255E","\u255F",
+  "\u255A","\u2554","\u2569","\u2566","\u2560","\u2550","\u256C","\u2567",
+  "\u2568","\u2564","\u2565","\u2559","\u2558","\u2552","\u2553","\u256B",
+  "\u256A","\u2518","\u250C","\u2588","\u2584","\u258C","\u2590","\u2580",
+  "\u03B1","\u00DF","\u0393","\u03C0","\u03A3","\u03C3","\u00B5","\u03C4",
+  "\u03A6","\u0398","\u03A9","\u03B4","\u221E","\u03C6","\u03B5","\u2229",
+  "\u2261","\u00B1","\u2265","\u2264","\u2320","\u2321","\u00F7","\u2248",
+  "\u00B0","\u2219","\u00B7","\u221A","\u207F","\u00B2","\u25A0","\u00A0"
+];
+
+function display_endoom(bytes, offset) {
+  var table = document.createElement("table");
+  table.className = "endoom-table";
+
+  for (var y = 0; y < 25; y++) {
+    var tr = document.createElement("tr")
+
+    for (var x = 0; x < 80; x++) {
+      const of = offset + (y * 80 + x) * 2;
+      const char = cp437_to_utf8[bytes[of]];
+      const info = bytes[of + 1];
+      const front_color = colors[info & 0b1111];
+      const back_color = colors[info >> 4 & 0b111];
+      const blink = info >> 6 & 0b10000000;
+
+      var td = document.createElement("td")
+      td.textContent = char;
+      td.style.color = front_color;
+      td.style.backgroundColor = back_color;
+      tr.appendChild(td)
+    }
+
+    table.appendChild(tr)
+  }
+
+  document.body.appendChild(table);
+}
